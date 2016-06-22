d105ac1d7730be5fbc41f145f635b72139102c15
Ruizhu Huang <rhuang@login1.wrangler.tacc.utexas.edu>
2016-06-21 19:08:58 -0500

    add comments to read_fasta_spark.py

--- EAGER-BioPython/read_fasta_spark.py
+++ EAGER-BioPython/read_fasta_spark.py
@@ -19,6 +19,7 @@ sc = SparkContext(conf = conf)
 input_1 = sys.argv[1]
 startTime = time.time()
 file_1= sc.textFile(input_1,minPartitions= 1000)
+# index with row numbers
 file_1_index = file_1.zipWithIndex()
 length = file_1_index.count()
 
@@ -30,14 +31,22 @@ def getSeqRow(x):
     if x[0][0]!='>':
         return x
 
+# todo: change the mapPartition to filter function to select non-none record
 id_row = file_1_index.map(getIdRow).mapPartitions(lambda a: [x for x in a if x is not None])
 
+# get row numbers eg. [1, 4, 8], length is 10
 id_pos = id_row.map(lambda a: a[0]).collect()
+
+# get a list of [1,1,1,4,4,4,4,8,8,8] by repeating [1,4,8,10]
 l = np.repeat(id_pos, np.diff(id_pos+[length])).tolist()
+
+# index with row number with l
 id_index = sc.parallelize(l).zipWithIndex()
 
+# get sequence rows
 seq_row=file_1_index.map(getSeqRow).mapPartitions(lambda a: [x for x in a if x is not None])
 
+# to get a list of [(1,(1,seq1_part1)),(1,(2,seq1_part2)),(1,(3,seq1_part3)),(4,(5,seq2_part1)),...]
 seq_row_re_index=id_index.map(lambda a: (a[1],a[0])).join(seq_row.map(lambda a: (a[1],str(a[0])))).map(lambda x: (x[1][0],(x[0],x[1][1])))
 
 
@@ -48,6 +57,8 @@ def f(x):
         s = s + a[1]
     return (x[0],s)
 
+# first get groupbykey [(1,[(1,seq1_part1),(2,seq1_part2),(3,seq1_part3)],(4,[5,seq2_part1),(6,seq2_part2)]),...]
+# then concatenate sequene string by order the row number before sequence, because combineByKey(add) mess up order
 seq_row_comb = seq_row_re_index.groupByKey().mapValues(list).map(f)
 
 id_seq = id_row.join(seq_row_comb).map(lambda a: a[1])
